f741f8a3175bfed9ea9850483e6f90e80a407c0d
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2022-01-18 00:00:57 +0300

    SOme Changes anad Client eroor

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,28 @@
-const express=require('express');
-const socketio=require('socket.io');
-const http=require('http');
-const app=express();
-const PORT =process.env.PORT||5000;
-const router =require('./router');
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
+const { callbackify } = require('util');
 
-const server=http.createServer(app);
-const io=socketio(server);
+const server = http.createServer(app);
+const io = socketio(server);
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('we have a new connection');
-    socket.on('disconnect',()=>{
+    socket.on('join', ({ name, room }) => {
+        console.log(name, room);
+        const error=true;
+        if(error){
+            callback({error:'error'});
+        }
+     
+    })
+    socket.on('disconnect', () => {
         console.log('User had left!!');
     })
 })
 app.use(router);
 
-server.listen(PORT,()=> console.log(`Server has started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
--- client/chating/src/components/Chat/Chat.js
+++ client/chating/src/components/Chat/Chat.js
@@ -1,9 +1,25 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import queryString from 'query-string';
-import  io from 'socket.io-client';
-import './Chat.css'
+import io from 'socket.io-client';
 
-const Chat = () => {
+import './Chat.css'
+let socket;
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+        //  console.log(name,room);
+        socket = io(ENDPOINT);
+        setRoom(room);
+        setName(name);
+        // console.log(socket);
+        socket.emit('join', { name: name, room: room }, ({ error }) => {
+            alert(error)
+        });
+    }, [ENDPOINT, location.search])
     return (
         <h1>Start Chat </h1>
     )
